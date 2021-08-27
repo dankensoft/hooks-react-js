2a7d006d090b4509a8291e7633184660c741908a
Danniels Castillo <danniels.magana@gmail.com>
2021-08-27 16:48:53 -0600

    Profundizando Hooks - useReducer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 // import {HookApp} from './HookApp';
 // import {CounterApp} from './components/01-useState/CounterApp';
@@ -12,14 +12,14 @@ import ReactDOM from 'react-dom';
 // import {Memorize} from './components/06-memos/Memorize';
 // import {MemoHook} from './components/06-memos/MemoHook';
 // import {CallbackHook} from './components/06-memos/CallbackHook';
-import {Padre} from './components/07-tarea-memo/Padre';
+// import {Padre} from './components/07-tarea-memo/Padre';
+import {TodoApp} from './components/08-useReducer/TodoApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Padre />
+    <TodoApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
- */
 
-import '../src/components/08-useReducer/intro-reducer';
+// import '../src/components/08-useReducer/intro-reducer';
